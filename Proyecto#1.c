de6e3f22595478967c69d5915d909cc58f60040c
UrielJr22 <62235428+UrielJr22@users.noreply.github.com>
2020-04-07 21:32:19 -0700

    Add files via upload

--- Proyecto#1.c
+++ Proyecto#1.c
@@ -1,59 +1,11 @@
 #include <stdio.h>
 
-void carta(){
-    int cart;
-    char op;
-    int suma;
-    int coca = 20;
-    int total
-    int nuevo
-
-    do{//Funcion do while para imprimir el menu
-    puts("Carta.");/*Menú con precios*/
-    puts("-Bebidas:");
-    puts("1.Coca cola-----$20.");
-    puts("2.Cafe-----$25.");
-    puts("3.Agua de sabor-----$20.");
-
-    puts(" ");
-    puts(" ");
-
-    puts("-Platillos:");
-    puts("Cada platillo incluye arroz blanco u opcion a sopa miso, elote, hongos shitake y huevo hervido o pochado.");
-    puts("4.Wakayama-Ramen $150 \n (Fideos de estilo chino acompañado de cebolletas y naruto, \n con una combinacion de caldo de huesos de cerdo y de caldo a base de salsa soja).");
-    puts("5.Okinawa Soba-Ramen $150 \n (Fideos soba acompañado de proteina de cerdo, algas y katsuobushi, \n con un caldo al estilo ramen y un topping de costilla de cerdo con hueso). ");
-    puts("6.Onomichi-Ramen $150 \n (Fideos finos acompañado de mariscos o pollo, \n con una combinacion de pollo y un poco de carne de cerdo para el caldo).");
-    puts("7.Sapporo Miso-Ramen $150 \n (Fideos finos acompañado de proteina de cerdo o res, \n con una combinado de pasta miso, genjibre y ajo para el caldo).");
-    puts("8.Hakata Tonkotsu-Ramen $150 \n (Fideos finos acompañado de proteina de res o cerdo,\n con una combinacion de mariscos con huesos de cerdo para el caldo).");
-    puts("Salir.");
-
-    puts(" ");
-    puts("Seleccione una opción. ");
-    scanf("%d",&cart);
-
-    switch(cart){
-    case 1 :
-
-        printf("Selecciono coca cola.\n");
-        printf("El costo es %d \n", coca);
-        system("pause");
-        puts("Desea seleccionar algo más (y/n)? ");
-        scanf("%c", &op);
-
-        break;
-
-        }
-
-    } while(cart!=1);
-
-
-}
-int main(){//primer menú
+int main(){//primer menï¿½
 
     int opc;
 
 
-    puts("Menu: ");
+    puts("~~~~~Menu~~~~~");
     puts("1.Carta.");
     puts("2.Editar carta.");
     puts("3.Salir. \n");
@@ -67,5 +19,55 @@ int main(){//primer menú
         carta();
         break;
     }
+}
+void carta(){
+    int cart;
+    char op;
+    int suma;
+    int coca = 20;
+    int total;
+    int nuevo;
+
+    do{//Funcion do while para imprimir el menu
+    puts("Carta.");/*Menï¿½ con precios*/
+    puts("-Bebidas:");
+    puts("1.Coca cola-----$20.");
+    puts("2.Cafe-----$25.");
+    puts("3.Agua de sabor-----$20.");
+
+    puts(" ");
+    puts(" ");
+
+    puts("-Platillos:");
+    puts("Cada platillo incluye arroz blanco u opcion a sopa miso, elote, hongos shitake y huevo hervido o pochado.");
+    puts("4.Wakayama-Ramen $150 \n (Fideos de estilo chino acompaï¿½ado de cebolletas y naruto, \n con una combinacion de caldo de huesos de cerdo y de caldo a base de salsa soja).");
+    puts("5.Okinawa Soba-Ramen $150 \n (Fideos soba acompaï¿½ado de proteina de cerdo, algas y katsuobushi, \n con un caldo al estilo ramen y un topping de costilla de cerdo con hueso). ");
+    puts("6.Onomichi-Ramen $150 \n (Fideos finos acompaï¿½ado de mariscos o pollo, \n con una combinacion de pollo y un poco de carne de cerdo para el caldo).");
+    puts("7.Sapporo Miso-Ramen $150 \n (Fideos finos acompaï¿½ado de proteina de cerdo o res, \n con una combinado de pasta miso, genjibre y ajo para el caldo).");
+    puts("8.Hakata Tonkotsu-Ramen $150 \n (Fideos finos acompaï¿½ado de proteina de res o cerdo,\n con una combinacion de mariscos con huesos de cerdo para el caldo).");
+    puts("Salir.");
+
+    puts(" ");
+    puts("Seleccione una opciï¿½n. ");
+    scanf("%d",&cart);
+
+    switch(cart){
+    case 1 :
+
+        printf("Selecciono coca cola.\n");
+        printf("El costo es %d \n", coca);
+        system("pause");
+        puts("Desea seleccionar algo mï¿½s (y/n)? ");
+        scanf("%c", &op);
+
+        break;
+
+        }
+
+    } while(cart!=1);
+
 
 }
+
+
+
